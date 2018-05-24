9e567e63993e5110b10414dea67821269baafdc4
ChrisHSWong <chriswonghonsai@gmail.com>
2018-05-24 23:50:58 +0800

    fixing errors

--- test/blink.js
+++ test/blink.js
@@ -3,7 +3,7 @@ var LED = new Gpio(4, 'out');
 var blinkInterval = setInterval(blinkLED, 250);
 
 function blinkLED(){
-	if (LED.readSync() === 0{
+	if (LED.readSync() === 0){
 	LED.writeSync(1);
 	}
 	else{
